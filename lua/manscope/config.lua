dd5684fea17ebcdf522723cdd20d4756ccb6f28c
bitcode <marcdrozrosario@gmail.com>
2024-05-12 19:58:27 -0400

    fixed db location in config.lua v2

--- lua/manscope/config.lua
+++ lua/manscope/config.lua
@@ -1,11 +1,8 @@
 local M = {}
 
--- Assuming use of stdpath to align with Neovim's recommended practice for storing data.
-local stdpath = vim.fn.stdpath('data')
-
 M.config = {
     -- Construct the path using stdpath to ensure it respects Neovim's directory structure.
-    database_path = stdpath .. '~/.local/share/nvim/lazy/manscope/lua/manscope/manscope.db',
+    database_path = '~/.local/share/nvim/lazy/manscope/lua/manscope/manscope.db',
     language = 'en',  -- Language of the man pages
     search_sensitivity = 'medium',  -- Example setting for search precision or fuzziness
 }
